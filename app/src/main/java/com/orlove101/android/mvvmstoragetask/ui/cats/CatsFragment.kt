b5a0c5e799542c69328bc3e25f8280c47cc6e898
OrLove101 <danyazerorebel@gmail.com>
2021-08-25 15:13:35 +0300

    docs: CatsViewModel implemented

--- app/src/main/java/com/orlove101/android/mvvmstoragetask/ui/cats/CatsFragment.kt
+++ app/src/main/java/com/orlove101/android/mvvmstoragetask/ui/cats/CatsFragment.kt
@@ -1,8 +1,77 @@
 package com.orlove101.android.mvvmstoragetask.ui.cats
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.orlove101.android.mvvmstoragetask.data.models.Cat
+import com.orlove101.android.mvvmstoragetask.databinding.FragmentCatsBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class CatsFragment: Fragment() {
+class CatsFragment: Fragment(), CatsAdapter.OnItemClickListener {
+    private var _binding: FragmentCatsBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: CatsViewModel by viewModels()
+    private var catsAdapter: CatsAdapter? = null
+    private var searchView: SearchView? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCatsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recyclerViewSetUp()
+    }
+
+    private fun recyclerViewSetUp() {
+        catsAdapter = CatsAdapter(this)
+
+        binding.apply {
+            recyclerView.apply {
+                adapter = catsAdapter
+                layoutManager = LinearLayoutManager(
+                    requireContext(),
+                    LinearLayoutManager.VERTICAL,
+                    false
+                )
+                setHasFixedSize(true)
+            }
+            ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+                override fun onMove(
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder,
+                    target: RecyclerView.ViewHolder
+                ): Boolean {
+                    return false
+                }
+
+                override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                    
+                }
+            })
+        }
+    }
+
+    override fun onItemClick(cat: Cat) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
--- app/src/main/java/com/orlove101/android/mvvmstoragetask/ui/cats/CatsViewModel.kt
+++ app/src/main/java/com/orlove101/android/mvvmstoragetask/ui/cats/CatsViewModel.kt
@@ -1,16 +1,88 @@
 package com.orlove101.android.mvvmstoragetask.ui.cats
 
-import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import com.orlove101.android.mvvmstoragetask.data.models.Cat
 import com.orlove101.android.mvvmstoragetask.persistence.CatsDao
 import com.orlove101.android.mvvmstoragetask.persistence.PreferencesManager
+import com.orlove101.android.mvvmstoragetask.persistence.SortOrder
+import com.orlove101.android.mvvmstoragetask.ui.ADD_TASK_RESULT_OK
+import com.orlove101.android.mvvmstoragetask.ui.EDIT_TASK_RESULT_OK
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class CatsViewModel @Inject constructor(
-    private val taskDao: CatsDao,
+    private val catsDao: CatsDao,
     private val preferencesManager: PreferencesManager,
     private val state: SavedStateHandle
 ): ViewModel() {
+    val searchQuery = state.getLiveData("searchQuery", "")
+
+    val preferencesFlow = preferencesManager.preferencesFlow
+
+    private val catsEventChannel = Channel<CatsEvent>()
+    val catsEvent = catsEventChannel.receiveAsFlow()
+
+    private val catsFlow = combine(
+        searchQuery.asFlow(),
+        preferencesFlow,
+    ) { query, filterPreferences ->
+        Pair(query, filterPreferences)
+    }.flatMapLatest { (query, filterPreferences) ->
+        catsDao.getCats(query, filterPreferences)
+    }
+
+    val cats = catsFlow.asLiveData()
+
+    fun onSortOrderSelected(sortOrder: SortOrder) = viewModelScope.launch {
+        preferencesManager.updateSortOrder(sortOrder)
+    }
+
+    fun onCatSelected(cat: Cat) {
+        viewModelScope.launch {
+            catsEventChannel.send(CatsEvent.NavigateToEditCatScreen(cat))
+        }
+    }
+
+    fun onCatSwiped(cat: Cat) {
+        viewModelScope.launch {
+            catsDao.delete(cat)
+            catsEventChannel.send(CatsEvent.ShowUndoDeleteCatMessage(cat))
+        }
+    }
+
+    fun onUndoDeleteClick(cat: Cat) {
+        viewModelScope.launch {
+            catsDao.insert(cat)
+        }
+    }
+
+    fun onAddNewCatClick() {
+        viewModelScope.launch {
+            catsEventChannel.send(CatsEvent.NavigateToAddCatScreen)
+        }
+    }
+
+    fun onAddEditResult(result: Int) {
+        when (result) {
+            ADD_TASK_RESULT_OK -> showCatSavedConfirmationMessage("Task added")
+            EDIT_TASK_RESULT_OK -> showCatSavedConfirmationMessage("Task updated")
+        }
+    }
+
+    private fun showCatSavedConfirmationMessage(text: String) = viewModelScope.launch {
+        catsEventChannel.send(CatsEvent.ShowCatSavedConfirmationMessage(text))
+    }
+
+    sealed class CatsEvent {
+        object NavigateToAddCatScreen: CatsEvent()
+        data class NavigateToEditCatScreen(val cat: Cat): CatsEvent()
+        data class ShowUndoDeleteCatMessage(val cat: Cat): CatsEvent()
+        data class ShowCatSavedConfirmationMessage(val msg: String): CatsEvent()
+    }
 }
