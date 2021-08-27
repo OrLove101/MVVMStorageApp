b2da68f19fe02fe4f9ba0d37d844436229ba530b
OrLove101 <danyazerorebel@gmail.com>
2021-08-27 09:46:56 +0300

    docs: implement AddEdit logic

--- app/src/main/java/com/orlove101/android/mvvmstoragetask/ui/cats/CatsFragment.kt
+++ app/src/main/java/com/orlove101/android/mvvmstoragetask/ui/cats/CatsFragment.kt
@@ -1,22 +1,26 @@
 package com.orlove101.android.mvvmstoragetask.ui.cats
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResultListener
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import com.orlove101.android.mvvmstoragetask.R
 import com.orlove101.android.mvvmstoragetask.data.models.Cat
 import com.orlove101.android.mvvmstoragetask.databinding.FragmentCatsBinding
+import com.orlove101.android.mvvmstoragetask.persistence.SortOrder
 import com.orlove101.android.mvvmstoragetask.util.exhaustive
+import com.orlove101.android.mvvmstoragetask.util.onQueryTextChanged
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class CatsFragment: Fragment(), CatsAdapter.OnItemClickListener {
@@ -24,6 +28,7 @@ class CatsFragment: Fragment(), CatsAdapter.OnItemClickListener {
     private val binding get() = _binding!!
     private val viewModel: CatsViewModel by viewModels()
     private var catsAdapter: CatsAdapter? = null
+    private var catsLayoutManager: LinearLayoutManager? = null
     private var searchView: SearchView? = null
 
     override fun onCreateView(
@@ -52,8 +57,15 @@ class CatsFragment: Fragment(), CatsAdapter.OnItemClickListener {
                 when (event) {
                     CatsViewModel.CatsEvent.NavigateToAddCatScreen -> TODO()
                     is CatsViewModel.CatsEvent.NavigateToEditCatScreen -> TODO()
-                    is CatsViewModel.CatsEvent.ShowCatSavedConfirmationMessage -> TODO()
-                    is CatsViewModel.CatsEvent.ShowUndoDeleteCatMessage -> TODO()
+                    is CatsViewModel.CatsEvent.ShowCatSavedConfirmationMessage -> {
+                        Snackbar.make(requireView(), event.msg, Snackbar.LENGTH_LONG).show()
+                    }
+                    is CatsViewModel.CatsEvent.ShowUndoDeleteCatMessage -> {
+                        Snackbar.make(requireView(), "Cat deleted", Snackbar.LENGTH_LONG)
+                            .setAction("Undo") {
+                                viewModel.onUndoDeleteClick(event.cat)
+                            }.show()
+                    }
                 }
             }.exhaustive
         }
@@ -61,15 +73,17 @@ class CatsFragment: Fragment(), CatsAdapter.OnItemClickListener {
 
     private fun recyclerViewSetUp() {
         catsAdapter = CatsAdapter(this)
+        catsLayoutManager = LinearLayoutManager(
+            requireContext(),
+            LinearLayoutManager.VERTICAL,
+            false
+        )
 
         binding.apply {
             recyclerView.apply {
                 adapter = catsAdapter
-                layoutManager = LinearLayoutManager(
-                    requireContext(),
-                    LinearLayoutManager.VERTICAL,
-                    false
-                )
+                layoutManager = catsLayoutManager
+
                 setHasFixedSize(true)
             }
             ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(0,
@@ -89,11 +103,52 @@ class CatsFragment: Fragment(), CatsAdapter.OnItemClickListener {
                 }
             }).attachToRecyclerView(recyclerView)
 
+            val dividerItemDecoration = DividerItemDecoration(recyclerView.context,
+                requireNotNull(catsLayoutManager?.orientation))
+            recyclerView.addItemDecoration(dividerItemDecoration)
+
             setFragmentResultListener("add_edit_request") { _, bundle ->
                 val result = bundle.getInt("add_edit_result")
 
                 viewModel.onAddEditResult(result)
             }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_fragment_cats, menu)
+
+        val searchItem = menu.findItem(R.id.action_search)
+
+        searchView = searchItem.actionView as SearchView
+
+        val pendingQuery = viewModel.searchQuery.value
+
+        if ( pendingQuery != null && pendingQuery.isNotEmpty() ) {
+            searchItem.expandActionView()
+            searchView?.setQuery(pendingQuery, false)
+        }
+
+        searchView?.onQueryTextChanged {
+            viewModel.searchQuery.value = it
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.action_sort_by_name -> {
+                viewModel.onSortOrderSelected(SortOrder.BY_NAME)
+                true
+            }
+            R.id.action_sort_by_age -> {
+                viewModel.onSortOrderSelected(SortOrder.BY_AGE)
+                true
+            }
+            R.id.action_sort_by_breed -> {
+                viewModel.onSortOrderSelected(SortOrder.BY_BREED)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 
@@ -103,6 +158,7 @@ class CatsFragment: Fragment(), CatsAdapter.OnItemClickListener {
 
     override fun onDestroyView() {
         super.onDestroyView()
+        searchView?.setOnQueryTextListener(null)
         _binding = null
     }
 }
